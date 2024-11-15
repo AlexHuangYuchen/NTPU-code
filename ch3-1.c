0053fbb85da3c361ff6dc38369db1586f938d39f
Alex_yuchen <123575452+AlexHuangYuchen@users.noreply.github.com>
2024-11-15 15:18:33 +0800

    Add files via upload

---
+++ ch3-1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a;
+    int s[5];
+    scanf("%d",&a);
+    for(int i=0;i<5;i++){
+        s[i]=a%10;
+        a=a/10;
+    }
+    if((s[0]==s[4])&&(s[1]==s[3])){
+        printf("1");
+    }
+    else{
+        printf("0");
+    }
+}
