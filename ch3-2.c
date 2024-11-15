91420ea3fff9d3db01d70a2ec5505d476207bdc0
Alex_yuchen <123575452+AlexHuangYuchen@users.noreply.github.com>
2024-11-15 15:20:24 +0800

    Add files via upload

---
+++ ch3-2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int year;
+    scanf("%d",&year);
+    year+=1911;
+    if((year%400==0)||(year%4==0)&&(year%100!=0)){
+        printf("1");
+    }
+    else{
+        printf("0");
+    }
+
+}
