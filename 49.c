f5b5868d4a33b034b7b0322d035aac2f420c3a33
Alex_yuchen <123575452+AlexHuangYuchen@users.noreply.github.com>
2024-11-20 01:37:02 +0800

    Create  49.c

---
+++ 49.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+        int n;
+        scanf("%d",&n);
+        for(int i=1;i<=n;i++){
+            printf("*");
+            if(i==1 || i==n){
+               for(int j=2;j<n;j++){
+                   printf("*");
+                }
+             }
+             else{
+                 for(int j=2;j<n;j++){
+                   printf(" ");
+                }
+             }
+            printf("*");
+            printf("\n");
+        }
+}
