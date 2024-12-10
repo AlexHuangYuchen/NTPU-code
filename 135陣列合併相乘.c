23f1327b5c17b09aea0777f0a272fc7437b0cfeb
Alex_yuchen <123575452+AlexHuangYuchen@users.noreply.github.com>
2024-12-10 23:37:10 +0800

    Add files via upload

---
+++ 135陣列合併相乘.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void array_mul(int *a, int *b, int size) {
+    for(int i=0;i<size;i++){
+        a[i]=a[i]*b[i];
+    }
+}
+int main(){
+    int data1[4];
+    int data2[4];
+    int size = sizeof(data1) / sizeof(data1[0]);
+    for(int i=0;i<4;i++){
+          scanf("%d", &data1[i]);
+    }
+    for(int i=0;i<4;i++){
+          scanf("%d", &data2[i]);
+    }
+    array_mul(data1, data2, size);
+    for(int i=0; i<size; i++) {
+        printf("%d ", data1[i]);
+    }
+    printf("\n");
+}
