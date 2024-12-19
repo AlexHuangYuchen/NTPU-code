53d4562dae8d6ea3a67347e24d449620349e835a
Alex_yuchen <123575452+AlexHuangYuchen@users.noreply.github.com>
2024-12-19 20:11:51 +0800

    Add files via upload

---
+++ 170. 河內塔遞迴版.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void hnr(int n, char A, char B, char C) {
+    if(n==1){
+        printf("盤子從%c移到%c\n",A,C);
+        return;
+    }
+    hnr(n-1,A,C,B);
+    printf("盤子從%c移到%c\n",A,C);
+    hnr(n-1,B,A,C);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    hnr(n, 'A', 'B', 'C');
+    return 0;
+}
